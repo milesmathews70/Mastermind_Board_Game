8ceca8bdbd0b61a8e57505d070050d10b4875565
milesmathews70 <milesm2@illinois.edu>
2018-11-30 22:51:19 -0600

    edited.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        CodeBreaker miles = new CodeBreaker("Miles");
+        Scanner in = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = in.nextLine();
+        CodeBreaker player = new CodeBreaker(name);
     }
 }
